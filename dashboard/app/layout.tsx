1febca0baad48ef47bf354aad2bc6c23ae27f23e
Daniel Pavel <daniel.pavel19@gmail.com>
2024-03-14 22:03:32 +0200

    add navbar + logo

--- dashboard/app/layout.tsx
+++ dashboard/app/layout.tsx
@@ -1,13 +1,11 @@
-import { ThemeSwitcher } from '@/components/theme-switcher'
-import { routes } from '@/config/routes'
 import { cn } from '@nextui-org/system'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
 
 import { Providers } from '../components/providers'
 import './globals.css'
 import type { ReactNode } from 'react'
+import MayaNavbar from '@/components/ui/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,14 +20,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" suppressHydrationWarning>
       <body className={cn(inter.className, 'flex h-screen flex-col')}>
         <Providers>
-          <nav className="flex w-full items-center justify-between border-b border-b-primary-100 p-5">
-            <h1 className="text-xl font-semibold">
-              <Link href={routes.home}>ZK Dashboard</Link>
-            </h1>
-            <div className="flex items-center justify-between space-x-4">
-              <ThemeSwitcher />
-            </div>
-          </nav>
+          <MayaNavbar />
           {children}
         </Providers>
       </body>
---
+++ dashboard/components/icons.tsx
@@ -0,0 +1,56 @@
+function MenuIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg
+      width="18"
+      height="16"
+      viewBox="0 0 18 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect width="18" height="2" fill="black" />
+      <rect y="7" width="18" height="2" fill="black" />
+      <rect y="14" width="18" height="2" fill="black" />
+    </svg>
+  )
+}
+
+function CloseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg
+      width="18"
+      height="19"
+      viewBox="0 0 18 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect y="8.72803" width="18" height="2" fill="black" />
+      <rect
+        x="8"
+        y="18.728"
+        width="18"
+        height="2"
+        transform="rotate(-90 8 18.728)"
+        fill="black"
+      />
+    </svg>
+  )
+}
+
+export function MenuIconDynamic({ isMenuOpen }: { isMenuOpen: boolean }) {
+  return (
+    <div className="relative">
+      <div
+        className={`$absolute inset-0 transition-transform duration-500 ease-in-out ${isMenuOpen ? 'rotate-180 opacity-0' : 'rotate-0 opacity-100'}`}
+      >
+        <MenuIcon />
+      </div>
+      <div
+        className={`absolute inset-0 transition-transform duration-500 ease-in-out ${isMenuOpen ? 'rotate-45 opacity-100' : '-rotate-45 opacity-0'}`}
+      >
+        <CloseIcon />
+      </div>
+    </div>
+  )
+}
