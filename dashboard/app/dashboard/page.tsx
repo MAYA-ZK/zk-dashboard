2212327a9f184066bcda0be1d5945e17f45117e5
Kristián Kostecký <kostecky.kristian@gmail.com>
2024-03-25 13:11:59 +0100

    add work in progress message (#8)

--- dashboard/app/dashboard/page.tsx
+++ dashboard/app/dashboard/page.tsx
@@ -1,3 +1,7 @@
 export default async function Page() {
-  return null
+  return (
+    <div className="flex size-full items-center justify-center text-center">
+      Work in progress... <br /> Check the specific rollups from top navigation.
+    </div>
+  )
 }
