f36273d078e0b5e5727b3aa521f0728931db9093
Kristián Kostecký <kostecky.kristian@gmail.com>
2024-03-30 08:43:59 +0100

    add refresh rate limit for materialized views

--- synchronizer/sync.ts
+++ synchronizer/sync.ts
@@ -8,20 +8,25 @@ import { syncPolygonZkEvm } from './polygon-zk-evm/sync'
 import { syncScroll } from './scroll/sync'
 import { syncZkSyncEra } from './zk-sync-era/sync'
 
-const SLEEP_FOR = 20 * 60 * 1000 // 20 minutes
+const SLEEP_FOR = 20 * 60 * 1_000 // 20 minutes
+const REFRESH_RATE = 6
 
-export async function sync() {
+export async function sync(runNumber = 0) {
   logger.info('START SYNCING')
 
   await syncEthUsdPrices()
   await syncScroll()
-  await refreshScrollMaterializedViews()
   await syncZkSyncEra()
-  await refreshZkSyncEraMaterializedViews()
   await syncPolygonZkEvm()
-  await refreshPolygonZkEvmMaterializedViews()
 
-  logger.info(`DONE SYNCING SLEEPING FOR ${SLEEP_FOR / 60 / 1000} MINUTES...`)
+  if (runNumber >= REFRESH_RATE) {
+    // Some views take long time to refresh,
+    // there is no need to refresh so frequently, since we show > 1 day data
+    await refreshScrollMaterializedViews()
+    await refreshZkSyncEraMaterializedViews()
+    await refreshPolygonZkEvmMaterializedViews()
+  }
+  logger.info(`DONE SYNCING SLEEPING FOR ${SLEEP_FOR / 60 / 1_000} MINUTES...`)
   await new Promise((resolve) => setTimeout(resolve, SLEEP_FOR))
-  await sync()
+  await sync(runNumber >= REFRESH_RATE ? 0 : runNumber + 1)
 }
