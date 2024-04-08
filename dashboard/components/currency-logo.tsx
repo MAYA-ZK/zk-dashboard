1bf3a52b815a702356032b6f9da3cd1c91282b8e
Daniel Pavel <daniel.pavel19@gmail.com>
2024-04-08 18:22:41 +0300

    changed currency logo to use image

--- dashboard/components/currency-logo.tsx
+++ dashboard/components/currency-logo.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image'
+
 import type { Currency } from '@zk-dashboard/common/lib/currency'
-import EthereumLogo from '@zk-dashboard/dashboard/public/ethereum-logo.svg'
-import UsdcLogo from '@zk-dashboard/dashboard/public/usdc-logo.svg'
 
 export function CurrencyLogo({ currency }: { currency: Currency }) {
-  return currency === 'usd' ? (
-    <UsdcLogo width={20} height={20} />
-  ) : (
-    <EthereumLogo width={20} height={20} />
+  return (
+    <Image
+      src={`${currency === 'usd' ? '/usdc-logo.svg' : '/ethereum-logo.svg'}`}
+      alt={`${currency}-logo`}
+      width={20}
+      height={20}
+    />
   )
 }
